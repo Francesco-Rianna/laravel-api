c8330e05c0cee386e14892429da6e7a96b0f8832
Francesco-Rianna <maradonino98@hotmail.it>
2024-06-12 19:09:53 +0200

    a dd index method to Api\ProjectController returning all projects in JSON

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -4,8 +4,19 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Project;
 
 class ProjectController extends Controller
 {
-    //
+    public function index()
+    {
+        $projects= Project::all();
+        // dd($projects);
+        return response()->json([
+             'succes'=> true,
+             'results'=> $projects
+
+            ]
+        );
+    }
 }
